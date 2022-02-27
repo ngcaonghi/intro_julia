507343275faf898e3eeab324c8634a042a1b696f
ngcaonghi <nghi.nguyen.180028@student.fulbright.edu.vn>
2022-02-28 02:33:59 +0700

    Add hello world

--- examples/helloWorld.jl
+++ examples/helloWorld.jl
@@ -1,1 +1,10 @@
-print("Hello world!")
+"""
+Doc-string, which is similar to Python.
+"""
+function helloworld()
+    # comment, which is similar to Python.
+    print("Hello world!")
+end
+
+# run script
+helloworld()
